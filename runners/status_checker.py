66debde28105fb2e4165ed3963afed85d28a5e74
Catalin Dinuta <43060213+dinuta@users.noreply.github.com>
2020-10-23 16:08:12 +0300

    Stream out or err (#2)
    * added out and err streaming

--- runners/status_checker.py
+++ runners/status_checker.py
@@ -1,4 +1,8 @@
+import os
 import time
+
+from utils.command_hasher import CommandHasher
+from utils.io_utils import IOUtils
 
 
 class StatusChecker:
@@ -8,12 +12,16 @@ class StatusChecker:
         self.service = service
         self.description = {}
         self.cmds_finished = {}
+        self.f_seek = {}
+        self.cmd_hasher = CommandHasher()
 
     def check_progress(self, poll_interval):
+        self.__poll_and_save()
+        self.__check_if_scheduled()
+        self.__init_f_seek()
         while True:
             self.__poll_and_save()
-            self.__check_if_scheduled()
-            self.__mark_and_print_if_finished()
+            self.__stream_out_err()
             if self.__check_finished():
                 return self.__get_global_status()
             time.sleep(poll_interval)
@@ -25,26 +33,34 @@ class StatusChecker:
             pass
 
     def __print_progress(self, cmd):
-        details = self.description.get('commands').get(cmd).get('details')
-        print(f">> {cmd}\n")
-        print(details.get('out') if details.get('err') == "" else details.get('err') + "\n")
-        print(f"Exit code: {details.get('code')}\n")
+        if len(self.f_seek[cmd]) == 0:
+            self.f_seek[cmd] = [IOUtils.get_fh_for_read(self.cmd_hasher.get_cmd_for_file_encode_str(cmd, ".out")),
+                                IOUtils.get_fh_for_read(self.cmd_hasher.get_cmd_for_file_encode_str(cmd, ".err"))]
+        out, err = self.f_seek[cmd][0].read().rstrip(), self.f_seek[cmd][1].read().rstrip()
+        self.f_seek[cmd][0].seek(0, os.SEEK_END), self.f_seek[cmd][1].seek(0, os.SEEK_SET)
+        if out == "" and err == "":
+            return
+        print(out if err == "" else err)
 
-    def __mark_and_print_if_finished(self):
+    def __stream_out_err(self):
         commands = self.description.get('commands').keys()
         for cmd in commands:
-            if self.description.get('commands').get(cmd).get('status') == 'finished' \
-                    and cmd not in self.cmds_finished:
-                self.cmds_finished[cmd] = self.description.get('commands').get(cmd).get('details').get('code')
-                self.__print_progress(cmd)
+            out_file = self.cmd_hasher.get_cmd_for_file_encode_str(cmd, ".out")
+            err_file = self.cmd_hasher.get_cmd_for_file_encode_str(cmd, ".err")
+            try:
+                IOUtils.write_to_file(out_file, self.description.get('commands').get(cmd).get('details').get('out'))
+                IOUtils.write_to_file(err_file, self.description.get('commands').get(cmd).get('details').get('err'))
+                self.__print_progress(cmd=cmd)
+            except Exception as e:
+                continue
 
     def __check_finished(self):
         return self.description.get('finished')
 
     def __get_global_status(self):
-        commands = self.cmds_finished.keys()
+        commands = self.description.get('commands').keys()
         for cmd in commands:
-            if self.cmds_finished.get(cmd) != 0:
+            if self.description.get('commands').get(cmd).get('details').get('code') != 0:
                 return 1
         return 0
 
@@ -52,3 +68,6 @@ class StatusChecker:
         if len(self.description.get('commands')) == 0:
             raise Exception("Exception: ({})".format("Error in the agent. The commands are not scheduled. "
                                                      "Check if you have 'start.py' in the path\n"))
+
+    def __init_f_seek(self):
+        self.f_seek = {key: [] for key in self.description.get('commands').keys()}
---
+++ utils/command_hasher.py
@@ -0,0 +1,19 @@
+import base64
+
+from constants.env_init import EnvInit
+
+
+class CommandHasher:
+    @staticmethod
+    def get_cmd_for_file_encode_list(command, suffix):
+        return EnvInit.CMD_DETACHED_STREAMS + "/" + \
+               base64.b64encode(command[0].encode("UTF-8")).decode("UTF-8") + suffix
+
+    @staticmethod
+    def get_cmd_for_file_encode_str(command, suffix):
+        return EnvInit.CMD_DETACHED_STREAMS + "/" + \
+               base64.b64encode(command.encode("UTF-8")).decode("UTF-8") + suffix
+
+    @staticmethod
+    def get_cmd_for_file_decode(command):
+        return ""
