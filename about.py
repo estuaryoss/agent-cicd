704d5ef229246dfeb2807301841e1b24f743d962
Catalin Dinuta <constantin.dinuta@gmail.com>
2021-02-01 01:34:43 +0200

    feature to execute syetm commands on the cicd

--- about.py
+++ about.py
@@ -1,6 +1,6 @@
 properties = {
     "name": "estuary-cicd",
-    "version": "0.2.4",
+    "version": "0.2.5",
     "description": "Execute your CI/CD flows",
     "author": "Catalin Dinuta",
     "platforms": ["Linux", "Mac", "Windows"],
